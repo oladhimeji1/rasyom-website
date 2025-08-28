91ec5470451a9de62b7c40a30af9b5537feff9f5
Godstime Pious <141561131+GTech23@users.noreply.github.com>
2025-08-28 14:09:25 +0000

    Change Bg color for button on About Route

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -55,7 +55,7 @@ export default function AboutPage() {
               </p>
               <Link
                 href="/contact"
-                className="inline-block bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors"
+                className="inline-block bg-[rgb(243,92,39)] text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors"
               >
                 Get In Touch
               </Link>
