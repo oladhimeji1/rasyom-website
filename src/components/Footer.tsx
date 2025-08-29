8bb4f9fdad7cb0540c52cf85746ef10aad244509
Godstime Pious <141561131+GTech23@users.noreply.github.com>
2025-08-29 10:04:03 +0000

    Fixed UI Issue on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#171A1E] text-gray-400 py-16">
       <div className="container mx-auto px-4">
-        <div className="flex justify-between gap-8">
+        <div className="flex flex-col md:flex-row justify-between gap-8">
           {/* Company Info */}
           <div className="mb-8 md:mb-0">
             <div className="flex items-center mb-4">
